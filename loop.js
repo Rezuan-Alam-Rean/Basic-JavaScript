527013faeacfb1200529e778c075ca6559178ee4
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2025-01-22 20:26:12 +0600

    added loop

--- loop.js
+++ loop.js
@@ -1,6 +1,6 @@
 const name = ["a", "b", "c", "d", "e" ]
 
-// for loop to iterate over array
+// for loop to iterate over array 
 for (let index = 0; index < 30; index++) {
     
     console.log(index);
