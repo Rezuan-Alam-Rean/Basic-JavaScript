6bc2a8979d4f400faa88a02c93a9a0212f8bded4
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2025-01-24 23:06:08 +0600

    added loop

--- loop.js
+++ loop.js
@@ -1,15 +1,28 @@
-const name = ["a", "b", "c", "d", "e" ]
+// const name = ["a", "b", "c", "d", "e" ]
 
-// for loop to iterate over array 
-for (let index = 0; index < 30; index++) {
+// // for loop to iterate over array 
+// for (let index = 0; index < 30; index++) {
     
-    console.log(index);
+//     console.log(index);
     
     
-}
+// }
 
-while (true) {
+// while (true) {
     
-    let index = 0; index < 30; index++
-    console.log(index);
+//     let num = Math.ceil(Math.random() * 100);
+    
+    
+//     if (num  === 99) {
+//         break;
+//     }
+//     console.log(num);
+// }
+
+ 
+do {
+    console.log("done");
+    
+} while (false) {
+    
 }
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,10 @@
- // if (index == name.length) {
-    //     index = 0 ;
-    // }
+while (true) {
+    
+    let num = Math.ceil(Math.random() * 100);
+    
+    console.log(num);
+
+    if (num = 99) {
+        break;
+    }
+}
