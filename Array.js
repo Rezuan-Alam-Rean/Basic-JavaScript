2c60a3e53f88113f5ea831a51506b9aa619c5904
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2025-01-26 23:59:35 +0600

    added loop

---
+++ Array.js
@@ -0,0 +1,10 @@
+// array
+
+const array = ["A", "b", "C", "d", "e", "F", "g", ]
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index], array[index].toLowerCase());
+    
+    
+}
+
--- loop.js
+++ loop.js
@@ -26,3 +26,14 @@
 // } while (false) {
     
 // }
+
+
+// array
+
+const array = ["A", "b", "C", "d", "e", "F", "g", ]
+
+for (let index = 0; index < array.length; index++) {
+    console.log(array[index], array[index].toLowerCase());
+    
+    
+}
