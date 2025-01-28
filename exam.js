b5ce1137d7ca7fd8157706423863c16b893380f5
Rezuan-Alam-Rean <rezuanrean.cse@gmail.com>
2025-01-28 21:16:20 +0600

    added loop

--- exam.js
+++ exam.js
@@ -24,6 +24,7 @@ const array = [
 //  let suma = 0;
 //  let sumb = 0;
 //  let sumc = 0;
+//  let result = {};
 
 //     for (let index = 0; index < array.length; index++) {
 
@@ -33,11 +34,12 @@ const array = [
 
 //         sumc += array[index].c || 0
         
+//         console.log(array[0]);
         
         
 //     }
 
-    // console.log(array[0]);
+//     console.log(array[0]);
 
 
     
@@ -45,21 +47,44 @@ const array = [
 // console.log(suma, sumb, sumc);
 
 
-const result = {};
-   for (var i = 0; i < array.length; i++) {
-     array[i]
-     for (var prop in array[i]) {
+// const result = {};
+//    for (var i = 0; i < array.length; i++) {
+//      array[i]
+//      for (var prop in array[i]) {
        
-       if(!result[prop]){
-         result[prop] = 0
-       }
-       result[prop] += array[i][prop]
+//        if(!result[prop]){
+//          result[prop] = 0
+//        }
+//        result[prop] += array[i][prop]
         
-     }
-   }
+//      }
+//    }
    
-   console.log(result)
+//    console.log(result)
    
+
+const result = {}; // Object to store the sums
+
+// Outer loop to iterate through the array
+for (let i = 0; i < array.length; i++) {
+  const obj = array[i];
+
+  // Inner loop to go through the properties of each object
+  const keys = Object.keys(obj); // Get the keys of the current object
+  for (let j = 0; j < keys.length; j++) {
+    const key = keys[j];
+    const value = obj[key];
+
+    // Add the value to the result or initialize it
+    if (result[key]) {
+      result[key] += value;
+    } else {
+      result[key] = value;
+    }
+  }
+}
+
+console.log(result);
 
 
 
